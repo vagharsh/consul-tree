30df8eb9fcf8dcd9428e637d5a6837ef87ce3af3
vagharsh <vagharsh@users.noreply.github.com>
2017-08-07 16:55:27 +0400

    Version 3.0
    - Import and Export functionality (https://github.com/vagharsh/consul-tree/issues/2)
    - Fixed the issue when creating a key or a folder while doing a right click on the key and not a folder (https://github.com/vagharsh/consul-tree/issues/1)
    - Notifies if there is not root for the tree (https://github.com/vagharsh/consul-tree/issues/3)
    - Some improvements in the coding

--- index.php
+++ index.php
@@ -53,20 +53,23 @@
             <form class="form-horizontal">
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-1 control-label">Search  : </label>
-                    <div class="col-sm-10">
-                        <input type="text" id="plugins4_q" value="" class="input form-control" style="margin:0em auto 1em auto;  padding:4px; border-radius:4px; border:1px solid silver;">
+                    <div class="col-sm-8">
+                        <input type="text" id="searchInputId" value="" class="input form-control" data-toggle="tooltip" data-placement="left" title="Tooltip on left" style="margin:0em auto 1em auto;  padding:4px; border-radius:4px; border:1px solid silver;">
+                    </div>
+                    <div class="col-sm-2">
+                        <button type="button" id="importExportBtnId" class="btn btn-primary" data-toggle="modal" data-target="#importExportModalId">Import / Export</button>
                     </div>
                 </div>
             </form>
 
         </div>
         <div class="row">
-            <div id="lazy" class="well col-md-5 padded-right-middle"></div>
+            <div id="ConsulTree" class="well col-md-5 padded-right-middle"></div>
             <div class="col-md-6 border-left" style="position: fixed; left: 610px; width: 568px;">
                 <textarea class="form-control update-control hidden" id="cKeyValue" rows="8"></textarea>
                 <br>
                 <button type="button" id="valueUpdateBtnId" class="btn btn-primary update-control hidden" >Update</button>
-                <span class="update-control hidden" style="color: #9f9f9f">&nbsp;|&nbsp;To create an element, right click on the tree.</span>
+                <span class="update-control hidden" style="color: #737373">&nbsp;|&nbsp;To create an element, right click on the tree.</span>
             </div>
         </div>
     </div>
@@ -84,13 +87,13 @@
                 <form class="">
                     <div class="form-group">
                         <span for="keyInputId" class="control-label">Folder / Key Name : </span>
-                        <input type="text" class="form-control" id="keyInputId" aria-describedby="basic-addon1" value="">
+                        <input type="text" class="form-control" id="keyInputId" value="">
                         <input type="hidden" class="form-control" id="pathInputId" value="">
                     </div>
 
                     <div class="form-group">
-                        <span for="basic-addon1" class=" control-label">Path : </span>
-                        <textarea class="form-control" id="basic-addon1" readonly >@</textarea>
+                        <span for="pathDescribeID" class=" control-label">Path : </span>
+                        <textarea class="form-control" id="pathDescribeID" readonly >@</textarea>
                     </div>
                 </form>
 
@@ -104,23 +107,69 @@
         </div>
     </div>
     <p id="demo"></p>
+</div>
 
+<div class="modal fade" id="importExportModalId" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-md">
+        <div class="modal-content">
+            <div class="modal-header modal-header-danger">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"><strong>Import / Export Consul Data</strong></h4>
+            </div>
+            <div class="modal-body">
+                <form class="">
+                    <div class="form-group">
+                        <label>Export Consul Data : </label>
+                        <button type="button" id="exportConsulBtnId" class="btn btn-warning" >Export</button>
+                    </div>
+                    <div class="form-group">
+                        <label>Import Consul Data : </label>
+                        <input type="file" id="jsonInputFile">
+                    </div>
+                    <button type="button" id="importConsulBtnId" class="btn btn-info">Import</button>
+                    <span style="color: #737373">&nbsp;|&nbsp;Only applicable if the JSON file was exported from the Consul-tree.</span>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="noTreeModalId" tabindex="-1" role="dialog">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header modal-header-danger">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"><strong>No Data</strong></h4>
+            </div>
+            <div class="modal-body">
+                <span><strong>No Data</strong> was found on Consul, <strong>Create</strong> at a root form the consul-ui, or <strong>Import</strong> an existing Tree form a previous export</span>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
 </div>
 <p class="hidden" id="ajaxReturnFieldID"></p>
 <p class="hidden" id="ajaxReturnVFieldID"></p>
 <p class="hidden" id="ccTypeFieldID"></p>
 <p class="hidden" id="ccParentFieldID"></p>
 <p class="hidden" id="selectedNodeID"></p>
+<p class="hidden" id="gotNodeValue"></p>
+<a id="downloadAnchorElem" style="display:none"></a>
 
 <div class="page-footer">
-    <h6 style="text-align:center">Application version: 2.7 | Updated on: <?php echo date("F d Y", filemtime('index.php'));?></h6>
+    <h6 style="text-align:center">Application version: 3.0 | Updated on: <?php echo date("F d Y", filemtime('index.php'));?></h6>
 </div>
 <script>
     $(document).ready(function () {
         <?php
             require './config.php';
         ?>
-
         var tree = {
             'contextmenu' : {'items' : customMenu},
             'plugins': ['contextmenu', 'types', 'state', 'search'],
@@ -131,7 +180,53 @@
                 'data': []
             }
         };
+        function importConsul(){
+            var files = document.getElementById('jsonInputFile').files;
+            if (files.length <= 0) {
+                alert('JSON file must be selected first.');
+                return false;
+            }
 
+            var fr = new FileReader();
+
+            fr.onload = function(e) {
+                var result = JSON.parse(e.target.result);
+                var formatted = JSON.stringify(result, null, 2);
+
+                $.each( result, function( key, item ) {
+                    sendToConsul(item.key, item.value, false);
+                })
+            };
+            fr.readAsText(files.item(0));
+        }
+        function exportConsul(){
+            var srcPath = JSON.parse($('#ajaxReturnFieldID').text()),
+                arr = [], type, value,dataStr, dlAnchorElem;
+
+            $.each( srcPath, function( key, item ) {
+                var fullPath = consulUrl + item;
+
+                if (fullPath.substr(fullPath.length - 1) !== '/'){
+                    type = 'key';
+                    getValue(item);
+                    value = encodeURIComponent($('#gotNodeValue').text());
+                } else {
+                    value = null;
+                    type = 'folder';
+                }
+                arr.push({
+                    key: item,
+                    type: type,
+                    value: value
+                });
+            });
+
+            dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(arr));
+            dlAnchorElem = document.getElementById('downloadAnchorElem');
+            dlAnchorElem.setAttribute("href", dataStr);
+            dlAnchorElem.setAttribute("download", "consul-data.json");
+            dlAnchorElem.click();
+        }
         function parseCustomJson(data, tree) {
             var minlen = -1;
             var picked = "";
@@ -183,12 +278,18 @@
                     url: path
                 }
             }).done(function (data) {
-                data = data.sort();
-                if (generateTree == true){
-                    tree = parseCustomJson(data, tree);
-                    $('#lazy').jstree(tree);
+                if (data.length === 0){
+                    $('#noTreeModalId').modal('show');
+                    $('#searchInputId').attr('disabled', true);
                 } else {
-                    $('#ajaxReturnFieldID').text(JSON.stringify(data));
+                    $('#searchInputId').attr('disabled', false);
+                    data = data.sort();
+                    if (generateTree == true){
+                        tree = parseCustomJson(data, tree);
+                        $('#ConsulTree').jstree(tree);
+                    } else {
+                        $('#ajaxReturnFieldID').text(JSON.stringify(data));
+                    }
                 }
             })
         }
@@ -204,7 +305,11 @@
                     url: path
                 }
             }).done(function (data) {
-                obj.text(data);
+                if(obj) {
+                    obj.text(data);
+                } else {
+                    $('#gotNodeValue').text(data);
+                }
             });
         }
         function sendToConsul(path, value, reload) {
@@ -257,7 +362,7 @@
                         workingInst = $.jstree.reference(data.reference);
                         workingObj = workingInst.get_node(data.reference);
 
-                        $('#basic-addon1').text(workingObj.id);
+                        $('#pathDescribeID').text(workingObj.id);
                         $('#createNodeModalId').modal('show');
                     }
                 },
@@ -364,7 +469,6 @@
 
             return items;
         }
-
         function check4Key (){
             var inputLength = $('#keyInputId').val().length;
             if (inputLength > 0 ){
@@ -377,8 +481,18 @@
         }
 
         $('#createNodeModalId').on('shown.bs.modal', function (){
-            $('#basic-addon1').text($('#selectedNodeID').text());
-            $('#pathInputId').val($('#selectedNodeID').text());
+            var selectedNodePath = $('#selectedNodeID').text(), splittedArray, newPath;
+
+            if (selectedNodePath.substr(selectedNodePath.length -1) !== '/'){
+                splittedArray = selectedNodePath.split("/");
+                splittedArray.splice(splittedArray.length -1, 1);
+                newPath = splittedArray.join('/');
+                selectedNodePath = newPath + '/';
+            }
+            
+            $('#pathDescribeID').text(selectedNodePath);
+            $('#pathInputId').val(selectedNodePath);
+
             check4Key();
         });
 
@@ -388,11 +502,10 @@
 
             sendToConsul(path, value, true )
         });
+        $('#exportConsulBtnId').on('click', exportConsul);
+        $('#importConsulBtnId').on('click', importConsul);
 
-        $('#keyInputId').on('click', function (){
-            check4Key();
-        });
-
+        $('#keyInputId').on('click', check4Key);
         $('#keyInputId').on('keyup', function (){
             check4Key();
 
@@ -403,34 +516,34 @@
                    $('#inputKeyValueId').removeClass('hidden');
                 }
             }
-           $('#basic-addon1').text($('#pathInputId').val() + $('#keyInputId').val());
+           $('#pathDescribeID').text($('#pathInputId').val() + $('#keyInputId').val());
         });
-
         $('#createKeyBtnId').on('click', function (){
-            workingInst.create_node(workingObj, {}, "last", function (new_node) {
-                new_node = $('#keyInputId').val();
-                var nodeName = workingObj.id + new_node;
-
+            workingInst.create_node(workingObj, {}, "last", function () {
+                var nodeName = $('#pathDescribeID').val();
                 var nodeValue = $('#inputKeyValueId').val(),
                     splittedPath =  nodeName.split("/"),
                     toBeCreatedPath = '';
 
-                for (i = 0; i < splittedPath.length -1 ; i++) {
+                for (var i = 0; i < splittedPath.length -1 ; i++) {
                     toBeCreatedPath = toBeCreatedPath + splittedPath[i] + "/";
+                    // Create Folders
                     sendToConsul(toBeCreatedPath, nodeValue, false);
                 }
-                sendToConsul(nodeName, nodeValue, true);
+                // Create the key
+                if (nodeName.substr(nodeName.length - 1) !== '/') {
+                    sendToConsul(nodeName, nodeValue, true);
+                }
             });
         });
 
         var to = false, jsTreeObj;
-        $('#plugins4_q').keyup(function () {
-            if (to) {
-                clearTimeout(to);
-            }
+
+        $('#searchInputId').keyup(function () {
+            if (to) {clearTimeout(to)}
             to = setTimeout(function () {
-                var v = $('#plugins4_q').val();
-                $('#lazy').jstree(true).search(v);
+                var v = $('#searchInputId').val();
+                $('#ConsulTree').jstree(true).search(v);
             }, 250);
         });
 
@@ -457,7 +570,7 @@
         // fixing ends here
         //getTree(tree, true, false);
 
-        $('#lazy').on("select_node.jstree", function (e, data) {
+        $('#ConsulTree').on("select_node.jstree", function (e, data) {
             workingInst = $.jstree.reference(data.reference);
             var updateControl = $('.update-control');
 
