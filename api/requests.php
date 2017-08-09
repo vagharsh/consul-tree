6ac15d9f58c5138b571c78d6d699400be6a3aa9c
vagharsh <vagharsh@users.noreply.github.com>
2017-08-09 11:03:08 +0400

    re-arranging

---
+++ api/requests.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$method = $_POST['method'];
+$url = $_POST['url'];
+
+if (isset($_POST['value'])) {
+    $value = $_POST['value'];
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL,$url);
+
+if ($method == "PUT") {
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $value);
+} elseif ($method == "DELETE") {
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+}
+
+$result=curl_exec($ch);
+curl_close($ch);
+echo ($result);
