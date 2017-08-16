0d60318048fcc71953706911aaf810bd7e1fdf16
vagharsh <vagharsh@users.noreply.github.com>
2017-08-16 18:59:02 +0400

    v 4.0
    - added functions.php and done some modifications in handling the requests

--- api/requests.php
+++ api/requests.php
@@ -1,25 +1,56 @@
 <?php
 
-
 $method = $_POST['method'];
 $url = $_POST['url'];
+
+require 'functions.php';
+
+if (isset($_POST['consulUrl'])) {
+    $consulUrl = $_POST['consulUrl'];
+}
 
 if (isset($_POST['value'])) {
     $value = $_POST['value'];
 }
 
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_URL,$url);
-
-if ($method == "PUT") {
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $value);
-} elseif ($method == "DELETE") {
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+if (isset($_POST['parentId'])) {
+    $parentId = $_POST['parentId'];
 }
 
-$result=curl_exec($ch);
-curl_close($ch);
-echo ($result);
+if (isset($_POST['replace'])) {
+    $replaceWith = $_POST['replace'];
+}
+
+if ($method === 'DELETE') {
+    $decodedJson = json_decode($url);
+    if (!is_array($decodedJson)) {$decodedJson = [$decodedJson];}
+    foreach ($decodedJson as $item) {
+        $keyUrl = $consulUrl . $item;
+        echo (deleteFromConsul($keyUrl));
+    }
+} elseif ($method === 'PUT') {
+    echo (putInConsul($url, $value));
+} elseif ($method === 'BulkIMPORT') {
+    $decodedJson = json_decode($value);
+    foreach ($decodedJson as $item) {
+        $keyUrl = $url . $item->key;
+        echo (putInConsul($keyUrl, $item->value));
+    }
+} elseif ($method === 'CCP') {
+    $decodedJson = json_decode($url);
+    foreach ($decodedJson as $item) {
+        $lastChar = substr($item, -1);
+        $newPath = str_replace($parentId,$replaceWith,$item);
+        $fullUrl = $consulUrl . $newPath;
+        $sourceUrl = $consulUrl . $item;
+
+        if ($lastChar == '/'){
+            putInConsul($newPath, false);
+        } else {
+            $sourceUrl = $sourceUrl . "?raw";
+            putInConsul($fullUrl, getFromConsul($sourceUrl));
+        }
+    }
+} else {
+    echo (getFromConsul($url));
+}
