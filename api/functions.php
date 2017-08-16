be86e1085ed0d75c4907212aded5bcc8db5c3d64
vagharsh <vagharsh@users.noreply.github.com>
2017-08-16 18:59:51 +0400

    v 4.0
    - added functions.php for (put, get, delete) curl requests

---
+++ api/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+function putInConsul($url, $value) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+    if ($value != false){
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $value);
+    }
+    $result=curl_exec($ch);
+    curl_close($ch);
+    return ($result);
+}
+
+function getFromConsul($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return ($result);
+}
+
+function deleteFromConsul($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return ($result);
+}
