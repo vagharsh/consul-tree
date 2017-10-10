3e64c748653c10c369639fe55621f6153b015335
Vagharsh Kandilian <vagharsh.kandilian@synisys.com>
2017-10-10 17:49:06 +0400

    v6.5

--- backend/consul.php
+++ backend/consul.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 session_start();
 if(empty($_SESSION["authenticated"]) || $_SESSION["authenticated"] != 'true') {
     header('Location: login.php');
--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 $password_err = null;
 $username_err = null;
 $rights_err = null;
