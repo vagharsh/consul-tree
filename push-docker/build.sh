e94312eaf98708df11d732d3d6ccdf6cf4f7a0ef
Vagharsh Kandilian <vagharsh@kandilian.com>
2024-03-14 10:55:26 +0400

    add build latest image

--- push-docker/build.sh
+++ push-docker/build.sh
@@ -3,6 +3,7 @@
 VERSION="$(cat ../backend/version)"
 VERSION="${VERSION:1}"
 
+latest_image_tag="vagharsh/consul-tree:latest"
 local_image_tag="vagharsh/consul-tree:$VERSION"
 web_image_tag="vagharsh/consul-tree:$VERSION-web"
 
@@ -11,5 +12,6 @@ docker build -t "$web_image_tag" .
 
 docker login --username=vagharsh
 
+docker push "$latest_image_tag"
 docker push "$local_image_tag"
 docker push "$web_image_tag"
